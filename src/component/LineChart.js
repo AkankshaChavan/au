3bb3d76f2069d6857c9351450416fd7eb562445f
akankshachavan <94160825+AkankshaChavan@users.noreply.github.com>
2025-02-17 12:18:24 +0000

    Line Chart

---
+++ src/component/LineChart.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+const LineChart = () => {
+  const options = {
+    title: {
+      text: "Monthly Sales Data",
+    },
+    xAxis: {
+      categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    },
+    yAxis: {
+      title: {
+        text: "Sales (in USD)",
+      },
+    },
+    series: [
+      {
+        name: "Sales",
+        data: [1200, 1800, 1500, 2000, 2200, 2500],
+      },
+    ],
+  };
+
+  return <HighchartsReact highcharts={Highcharts} options={options} />;
+};
+
+export default LineChart;
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,12 +3,14 @@ import Header from "../component/header";
 import Footer from "../component/footer";
 import Admin from "../component/admin";
 import ChUser from "../component/chUser";
+import LineChart from "../component/LineChart";
 
 function Dashboard() {
   return (
     <>
       <Header />
       <Admin />
+      <LineChart />
       <Footer />
     </>
   );
